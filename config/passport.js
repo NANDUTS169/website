fbf0f854921964280912233b64a1f01cdbb98805
NANDU <nandusreekandan169@gmail.com>
2026-06-21 15:05:08 +0530

    Fixed google auth callback URL

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL ,
       scope: ["profile", "email"], // Ensure email is included in the scope
     },
     async (accessToken, refreshToken, profile, done) => {
